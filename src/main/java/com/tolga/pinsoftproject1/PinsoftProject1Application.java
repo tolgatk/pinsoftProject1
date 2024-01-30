0afcbe8f59c6609c3823fd8ae4bedb2c70b671cb
Tolga Tok <114034636+tolgatk@users.noreply.github.com>
2024-01-31 00:03:51 +0300

    son

--- src/main/java/com/tolga/pinsoftproject1/PinsoftProject1Application.java
+++ src/main/java/com/tolga/pinsoftproject1/PinsoftProject1Application.java
@@ -1,7 +1,12 @@
 package com.tolga.pinsoftproject1;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
 
 @SpringBootApplication
 public class PinsoftProject1Application {
@@ -9,6 +14,17 @@ public class PinsoftProject1Application {
     public static void main(String[] args) {
         SpringApplication.run(PinsoftProject1Application.class, args);
     }
+
+
+//@Bean
+//public SpringLiquibase liquibase(DataSource dataSource) {
+//    SpringLiquibase liquibase = new SpringLiquibase();
+//    liquibase.setChangeLog("classpath:config/liquibase/changelog/master.xml");
+//    liquibase.setDataSource(dataSource);
+//    return liquibase;
+//}
+//
+
 
 
 
